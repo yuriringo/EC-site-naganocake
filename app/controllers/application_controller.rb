a9001ef55832e3b9d94798d57db6ec4314cf4ac4
EC2 Default User <ec2-user@ip-172-31-44-112.ap-northeast-1.compute.internal>
2023-05-07 04:22:58 +0000

    naganocakeのコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+ def after_sign_in_path_for(resource)
+  customer_path(params[:id])
+ end
+
+ protected
+
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+ end
+
 end
