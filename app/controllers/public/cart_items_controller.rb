06c212529ed048e575aefc561517c58b7bac94ff
EC2 Default User <ec2-user@ip-172-31-44-112.ap-northeast-1.compute.internal>
2023-05-18 12:34:38 +0000

    naganocakeのコード

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
+   @items=Item.all
   end
 
   def update
@@ -12,5 +13,10 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+  def cart_item_params
+   params.require(:cart_item).permit(:item_id, :amount)
   end
 end
