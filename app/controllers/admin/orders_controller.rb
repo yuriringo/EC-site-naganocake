d48cd24e1f98858b49425c95496c07876a9046d9
EC2 Default User <ec2-user@ip-172-31-44-112.ap-northeast-1.compute.internal>
2023-06-05 23:12:33 +0000

    naganocakeのコード途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,13 @@
 class Admin::OrdersController < ApplicationController
   def show
+   @order=Order.find(params[:id])
   end
 
   def update
+  end
+
+  private
+  def order_params
+   params.require(:order).permit(:name, :updated_at, :postal_code, :address, :payment_method, :total, :postage)
   end
 end
