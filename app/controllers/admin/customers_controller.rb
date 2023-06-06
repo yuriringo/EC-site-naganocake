732a9ab5c967d08e08c23d74750282b5e5a5bf84
EC2 Default User <ec2-user@ip-172-31-44-112.ap-northeast-1.compute.internal>
2023-06-06 23:28:17 +0000

    ECサイトのコード途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,7 +4,7 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-   @customer=Customer.find
+   @customer=Customer.find(params[:id])
   end
 
   def edit
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,8 @@ class OrderItem < ApplicationRecord
  belongs_to :order
  belongs_to :item
 
+ enum confectionary_status: { not_product: 0, product_wait: 1, producting: 2, completed: 3 }
+
  def sum_of_price
   item.add_tax_price * amount
  end
